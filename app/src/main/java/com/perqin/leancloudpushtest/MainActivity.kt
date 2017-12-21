132d1207f950577d41d73ff41998889e1e5493b7
perqin <perqinxie@gmail.com>
2017-12-21 14:12:26 +0800

    Send push message directly in app

--- app/src/main/java/com/perqin/leancloudpushtest/MainActivity.kt
+++ app/src/main/java/com/perqin/leancloudpushtest/MainActivity.kt
@@ -2,10 +2,21 @@ package com.perqin.leancloudpushtest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.avos.avoscloud.AVPush
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_test.setOnClickListener {
+            val avPush = AVPush()
+            val obj = JSONObject("{\"alert\": \"test\"}")
+            avPush.setPushToAndroid(true)
+            avPush.setData(obj)
+            avPush.sendInBackground()
+        }
     }
 }
